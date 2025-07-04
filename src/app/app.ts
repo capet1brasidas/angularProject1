66e706901000636b937a67e4bbcc0d4df85cdc3f
Capet-Zeyu-Lin <zlin0089@student.monash.edu>
2025-07-04 12:00:22 +1000

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angularStudy1';
+  protected title = 'angularStudy1,i am capet';
 }
